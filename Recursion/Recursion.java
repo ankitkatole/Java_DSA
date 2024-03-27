996fe1a7270edaafeeb2198c907abf1deed98bb2
ankitkatole <katoleankit06@gmail.com>
2024-03-27 14:45:59 +0530

    Recursion

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class Recursion {
     static public int factorial(int n){
@@ -23,7 +23,43 @@ public class Recursion {
         print(n-1);
         System.out.println(n);
     }
+
+
+    private static HashMap<Integer,Integer> memo = new HashMap<>();
+
+     public static int fibbanocci(int n){
+        if(n == 1 || n == 0){
+           return n;
+        }
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int res = fibbanocci(n-1) + fibbanocci(n-2);
+        memo.put(n, res);
+        return res;
+    }
+
+    public static Boolean isSorted(int arr[],int i){
+        if(i >= arr.length-1)//Base Case: Sorted array
+         return true;
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
+
+    public static int firstOccurence(int arr[],int key,int i){
+        if(i >= arr.length)
+            return -1;
+        if(arr[i] != key){
+            return firstOccurence(arr, key, i+1);
+        }
+        return i+1;
+    }
     public static void main(String[] args) {
-        System.out.println(sum(5));
+        int[] arr = {1,1,3,4,5,6};
+        System.out.println(firstOccurence(arr, 3, 0));
+        
     }
 }
