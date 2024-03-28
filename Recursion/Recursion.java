69141f8cdb9b0993369507956ef54dc53340c567
ankitkatole <katoleankit06@gmail.com>
2024-03-28 23:35:38 +0530

    Recursion :  Binary Search

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -57,9 +57,26 @@ public class Recursion {
         }
         return i+1;
     }
+
+    public static int binarySearch(int arr[],int key,int low,int high){
+        if(low>high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] > key){
+            return binarySearch(arr, key, low, mid-1);
+        }
+        else{
+            return binarySearch(arr, key, mid+1, high);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {1,1,3,4,5,6};
-        System.out.println(firstOccurence(arr, 3, 0));
+        // System.out.println(firstOccurence(arr, 3, 0));
+        System.out.println(binarySearch(arr, 4, 0, arr.length-1));
         
     }
 }
