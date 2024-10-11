40081bb4708f0e287e725aad5802af2206575d77
ankitkatole <katoleankit06@gmail.com>
2024-10-11 15:13:29 +0530

    Patterns P1

---
+++ Week-1/Pattern.java
@@ -0,0 +1,116 @@
+public class Pattern{
+
+    public static void hollowRectangle(int row, int col){
+        for(int i = 1;i <= row; i++){
+            for(int j = 1; j <= col; j++){
+                if(i==1 || i == row || j==1 || j == col){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void invertedHalfPyramid(int n){
+        int empty = n-1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<= n; j++){
+                if(j<=empty){
+                    System.out.print("  ");
+                }else{
+                    System.out.print("* ");
+                }
+            }
+            empty--;
+            System.out.println();
+        }
+    }
+
+    public static void invertedNumber(int n){
+        for(int i = n;i>0;i--){
+            for(int j = 1;j <= i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floydsTriangle(int n){
+        int num = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <=i;j++){
+                System.out.print(num+"  ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zeroOneTriangle(int n){
+        int flag;
+        for(int i = 1; i<=n;i++){
+            flag = i%2;
+            for(int j = 1; j<=i;j++){
+                System.out.print(flag+" ");
+                if(flag == 0){
+                    flag = 1;
+                }else{
+                    flag = 0;
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void zeroOneTriangle2(int n){
+
+        for(int i = 1; i<=n;i++){
+            for(int j = 1; j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterflyPattern(int n){
+        for(int i = 1; i<=n;i++){
+            for(int j= 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<= (2*(n-i));j++){
+                System.out.print("  ");
+            }
+            for(int j= 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i = n; i>=1;i--){
+            for(int j= 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<= (2*(n-i));j++){
+                System.out.print("  ");
+            }
+            for(int j= 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hollowRectangle(4,5);
+        invertedHalfPyramid(8);
+        invertedNumber(5);
+        floydsTriangle(5);
+        zeroOneTriangle(5);
+        zeroOneTriangle2(5);
+        butterflyPattern(4);
+    }
+}
