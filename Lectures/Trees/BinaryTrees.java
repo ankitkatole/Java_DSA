b3e9ef485902dd6d08b74bb15c2544fff9aeea78
ankitkatole <katoleankit06@gmail.com>
2025-01-13 07:48:00 +0530

    Binary Trees Lecture one

---
+++ Lectures/Trees/BinaryTrees.java
@@ -0,0 +1,128 @@
+import java.util.*;
+
+public class BinaryTrees {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        public static void preOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        public static void levelorder(Node root){
+            if(root == null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+
+        public static int height(Node root){
+            if(root == null){
+                return 0;
+            }
+            int lh = height(root.left);
+            int rh = height(root.right);
+            return Math.max(lh,rh)+1;
+        }
+
+        public static int countNodes(Node root){
+            if(root == null){
+                return 0;
+            }
+            return countNodes(root.left)+countNodes(root.right)+1;
+        }
+
+        public static int sum(Node root){
+            if(root == null){
+                return 0;
+            }
+            int leftSum = sum(root.left);
+            int rightSum = sum(root.right);
+            return leftSum+rightSum+root.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+        System.out.print("\nPreorder Sequence: ");
+        tree.preOrder(root);
+        System.out.print("\nInorder Sequence: ");
+        tree.inOrder(root);
+        System.out.print("\nPostorder Sequence: ");
+        tree.postOrder(root);
+        System.out.println("Level Order Traverse: ");
+        tree.levelorder(root);
+        System.out.println("Height of the tree: "+tree.height(root));
+        System.out.println("Number of nodes in the tree: "+tree.countNodes(root));
+        System.out.println("Sum of all nodes in the tree: "+tree.sum(root));
+    }
+}
