5d85e90d431d889d0d88e540c03fb820c3cfce8f
ankitkatole <katoleankit06@gmail.com>
2025-03-31 21:29:16 +0530

    WordBreak Problem

---
+++ Lectures/Tries/Problems.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+
+public class Problems {
+    /*
+     * Word Break Problem
+     * Given a string s and a dictionary of strings wordDict, return true if s can
+     * be segmented into a space-separated sequence of one or more dictionary words.
+     * Note that the same word in the dictionary may be reused multiple times in the
+     * segmentation.
+     * Example 1:
+     * Input: Words[]= {i,like,sam,samsung,mobile,ice} key= ilikesamsung
+     * Output: Yes
+     */
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEndOfWord;
+
+        TrieNode() {
+            isEndOfWord = false;
+            // Initialize all children to null
+            // This is done in the constructor to avoid null pointer exceptions
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static TrieNode root = new TrieNode();
+
+    public static void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public static boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isEndOfWord;
+    }
+
+    public static boolean wordbreak(String key){
+        // Base case: if the string is empty, return true
+        if (key.length() == 0) {
+            return true;
+        }
+        // Check if the string is present in the trie
+        if (search(key)) {
+            return true;
+        }
+        // Iterate through the string and check for all prefixes
+        // If any prefix is found in the trie, check for the remaining substring
+        // recursively
+        // This is the main logic of the word break problem
+        for(int i = 1; i <= key.length();i++){
+            if(search(key.substring(0, i)) && wordbreak(key.substring(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+        String key = "ilikesamsung";
+        for (String word : arr) {
+            insert(word);
+        }
+        if (wordbreak(key)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.println("----------------------");
+    }
+}
--- Lectures/Tries/Tries.java
+++ Lectures/Tries/Tries.java
@@ -1,17 +1,65 @@
 import java.util.*;
 
 public class Tries {
-    static class TrieNode{
+    static class TrieNode {
         TrieNode[] children = new TrieNode[26];
-        boolean isEndOfWord= false;
-        TrieNode(){
-            for(int i=0; i<26; i++){
+        boolean isEndOfWord;
+
+        TrieNode() {
+            isEndOfWord = false;
+            // Initialize all children to null
+            // This is done in the constructor to avoid null pointer exceptions
+            for (int i = 0; i < 26; i++) {
                 children[i] = null;
             }
         }
     }
+
     public static TrieNode root = new TrieNode();
+
+    public static void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public static boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isEndOfWord;
+    }
+
     public static void main(String[] args) {
-        String words[] = {"hello", "hell", "heaven", "heavy", "hero"};
+        String words[] = { "hello", "hell", "heaven", "heavy", "hero" };
+        System.out.println("## Words inserted into the trie. ##");
+        // Insert words into the trie
+        for (String word : words) {
+            insert(word);
+        }
+        System.out.println("----------------------");
+
+        // Search for words in the trie
+        String searchWords[] = { "hello", "hell", "heaven", "heavy", "hero", "her" };
+        System.out.println("## Words searched in the trie. ##");
+        for (String word : searchWords) {
+            if (search(word)) {
+                System.out.println(word + " is present in the trie.");
+            } else {
+                System.out.println(word + " is not present in the trie.");
+            }
+        }
+        System.out.println("----------------------");
     }
 }
