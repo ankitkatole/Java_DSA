ac1946702ecb0c26e73c8b6f6edc255b133adb4d
ankitkatole <katoleankit06@gmail.com>
2025-05-02 20:36:40 +0530

    Graphs 2

---
+++ Lectures/Graphs/Graph2.java
@@ -0,0 +1,121 @@
+import java.util.*;
+
+public class Graph2 {
+    static class Edge {
+        int src, dest;
+
+        Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        boolean[] visited = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i]) {
+                bfs_UTIL(graph, visited);
+            }
+        }
+    }
+
+    public static void bfs_UTIL(ArrayList<Edge>[] graph, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0); // Start BFS from vertex 0
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (!visited[curr]) {
+                System.out.print(curr + " ");
+                visited[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
+    }
+
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (detectCycleUtil(graph, vis, i, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] vis, int curr, int parent) {
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                if (detectCycleUtil(graph, vis, e.dest, curr)) {
+                    return true;
+                }
+            } else if (e.dest != parent) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBipartile(ArrayList<Edge>[] graph) {
+        int col[] = new int[graph.length];
+        for (int i = 0; i < col.length; i++) {
+            col[i] = -1;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (col[i] == -1) {
+                q.add(i);
+                col[i] = 0;
+                while (!q.isEmpty()) {
+                    int curr = q.poll();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
+                        if (col[e.dest] == -1) {
+                            col[e.dest] = 1 - col[curr];
+                            q.add(e.dest);
+                        } else if (col[e.dest] == col[curr]) {
+                            return false;
+                        } 
+                        
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int V = 4; // Number of vertices
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        System.out.println("Cycle Detection: " + detectCycle(graph));
+        System.out.println("Bipartite Check: " + isBipartile(graph));
+    }
+}
