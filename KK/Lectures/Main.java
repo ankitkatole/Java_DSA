394a91721036d5a398114fb817a61f8a9046e6c9
ankitkatole <katoleankit06@gmail.com>
2025-12-25 21:50:19 +0530

    KK

---
+++ KK/Lectures/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // int arr[] = new int[10];
+        // Scanner sc = new Scanner(System.in);
+        // for(int i = 0; i < arr.length; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
+        // for(int i = 0; i < arr.length; i++) {
+        //     System.out.print("arr["+i+"]=" + arr[i] + " ");
+        // }
+
+        
+    }
+}
